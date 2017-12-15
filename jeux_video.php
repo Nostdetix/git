66f2e3906ea1aaca7c8a14ad765c8a5efda3c3ce
Nostdetix <clement.gerigne@gmail.com>
2017-12-15 14:29:15 +0100

    Initial commit

---
+++ jeux_video.php
@@ -0,0 +1,28 @@
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=bdd;charset=utf8', 'root', '');
+
+}
+catch(Exception $e)
+{
+	
+        die('Erreur : '.$e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT * FROM jeux_video');
+
+while ($donnees = $reponse->fetch())
+{
+?>
+    <p>
+    <strong>Jeu</strong> : <?php echo $donnees['nom']; ?><br />
+    Le possesseur de ce jeu est : <?php echo $donnees['possesseur']; ?>, et il le vend à <?php echo $donnees['prix']; ?> euros !<br />
+    Ce jeu fonctionne sur <?php echo $donnees['console']; ?> et on peut y jouer à <?php echo $donnees['nbre_joueurs_max']; ?> au maximum<br />
+    <?php echo $donnees['possesseur']; ?> a laissé ces commentaires sur <?php echo $donnees['nom']; ?> : <em><?php echo $donnees['commentaires']; ?></em>
+   </p>
+<?php
+}
+
+$reponse->closeCursor(); 
+?>
